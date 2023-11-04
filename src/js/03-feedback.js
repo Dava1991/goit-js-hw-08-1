a601b430832062aee022cddcab9a1a5e7ac8b8e7
Dava1991 <schulzotto1990@gmail.com>
2023-11-04 11:51:54 +0200

    9

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,41 +1,41 @@
 import throttle from 'lodash.throttle';
 const STORAGE_KEY = 'feedback-form-state';
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  email: document.querySelector('.email-content'),
-  message: document.querySelector('.message-content'),
-};
 let dataSet = {};
-refs.form.addEventListener('input', throttle(inputValue, 500));
-function inputValue(evt) {
-  evt.preventDefault();
-  dataSet[evt.target.name] = evt.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataSet));
+const refs = {
+    inputForm: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input'),
+    message: document.querySelector('.feedback-form textarea')
+};
+refs.inputForm.addEventListener('input', throttle(onInputForm, 500));
+function onInputForm(e) {
+    dataSet[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(dataSet));
 }
 
-function storageData() {
-  const saveData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  console.log(saveData);
-  if (saveData) {
-    refs.email.value = saveData.email ?? [];
-    dataSet.email = saveData.email;
-    refs.message.value = saveData.message ?? [];
-    dataSet.message = saveData.message;
-  }
-}
-storageData();
+populateData();
+function populateData() {
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    console.log(savedData);
+  if (savedData) {
+      refs.email.value = savedData.email ?? [];
+      dataSet.email = savedData.email;
+      dataSet.message = savedData.message;
+      refs.message.value = savedData.message  ?? [];
+    };
+};
 
-refs.form.addEventListener('submit', onFormSubmit);
-function onFormSubmit(evt) {
-  if (refs.email.value === '' || refs.message.value === '') {
-    alert('Please enter all fields!');
-    return;
-  } else {
+refs.inputForm.addEventListener('submit', onFormSubmit);
+function onFormSubmit(e) {
+    if (refs.email.value === "" || refs.message.value === "") {
+        alert(`Please enter all fields!`);
+        return;
+    } else {
     console.log(dataSet);
-    evt.currentTarget.reset();
+    e.preventDefault();
+    e.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
     dataSet = {};
-    refs.email = '';
-    refs.message = '';
-  }
+    refs.email = "";
+    refs.message = "";
+     }
 }
